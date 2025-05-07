4d4ef0edfe34f915d11a2ef82b6e3ad5379d0aa5
Mher Mnatsakanyan <mher.mnatsakanyan03@gmail.com>
2025-05-07 08:21:53 +0200

    serie 5 init

---
+++ Übungen/Serie 5/task5_1.py
@@ -0,0 +1,19 @@
+import numpy as np # math functionality
+import matplotlib.pyplot as plt # plotting
+
+def phi(r):
+    return 0
+
+points = np.array([[-2,-2], [2,0], [0,-1], [-1,2]])
+values = np.array( [0.2,     0.6,   0.3,    0.5])
+
+fig = plt.figure(figsize=(8,7))
+
+
+
+
+
+
+
+# Always run show, to make sure everything is displayed.
+plt.show()
